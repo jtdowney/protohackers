15b6b4ec11ad137b2158213e71ad9b5fc8aa8eca
John Downey <jdowney@gmail.com>
2023-01-02 15:12:02 -0800

    Provide command line options for config

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod proxy;
 
 use std::{future::Future, net::Ipv4Addr};
 
+use argh::FromArgs;
 use tokio::{
     io::BufReader,
     net::{TcpListener, TcpStream},
@@ -13,21 +14,38 @@ use tracing::{debug, info, warn};
 
 use crate::proxy::read_proxy_proto;
 
+#[derive(FromArgs)]
+/// Protohackers
+struct Args {
+    /// read PROXY protocol header
+    #[argh(switch)]
+    proxy_protocol: bool,
+
+    /// start port
+    #[argh(option, short = 'p', default = "10000")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt::init();
     stable_eyre::install()?;
 
-    let problem0 = tokio::spawn(create_server(9000, problem0::handle));
-    let problem1 = tokio::spawn(create_server(9001, problem1::handle));
-    let problem2 = tokio::spawn(create_server(9002, problem2::handle));
+    let Args {
+        port,
+        proxy_protocol,
+    } = argh::from_env();
+
+    let problem0 = tokio::spawn(create_server(port, proxy_protocol, problem0::handle));
+    let problem1 = tokio::spawn(create_server(port + 1, proxy_protocol, problem1::handle));
+    let problem2 = tokio::spawn(create_server(port + 2, proxy_protocol, problem2::handle));
 
     let _ = tokio::join!(problem0, problem1, problem2);
 
     Ok(())
 }
 
-async fn create_server<F, Fut>(port: u16, handle: F) -> eyre::Result<()>
+async fn create_server<F, Fut>(port: u16, proxy_protocol: bool, handle: F) -> eyre::Result<()>
 where
     F: Fn(BufReader<TcpStream>) -> Fut + Send + Sync + Copy + 'static,
     Fut: Future<Output = eyre::Result<()>> + Send + 'static,
@@ -37,17 +55,22 @@ where
     info!("listening on on {bind:?}");
 
     loop {
-        let (stream, _) = listener.accept().await?;
+        let (stream, addr) = listener.accept().await?;
 
         tokio::spawn(async move {
             let mut stream = BufReader::new(stream);
-            match read_proxy_proto(&mut stream).await {
-                Ok(addr) => info!("connection from {addr}"),
-                Err(proxy::Error::EmptyHeader) => return,
-                Err(e) => {
-                    debug!(error = ?e, "failed to read proxy protocol");
-                    return;
+
+            if proxy_protocol {
+                match read_proxy_proto(&mut stream).await {
+                    Ok(addr) => info!("connection from {addr}"),
+                    Err(proxy::Error::EmptyHeader) => return,
+                    Err(e) => {
+                        debug!(error = ?e, "failed to read proxy protocol");
+                        return;
+                    }
                 }
+            } else {
+                info!("connection from {addr}");
             }
 
             if let Err(e) = handle(stream).await {
